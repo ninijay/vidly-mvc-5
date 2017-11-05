622f30a31486019bc521f9da92b755663d05d3b2
Mattia Ninivaggia <ninivmat@students.zhaw.ch>
2017-11-05 18:24:42 +0100

    Exercise 3 Done

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -1,45 +1,51 @@
 ﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
 using System.Web.Mvc;
 using Vidly.Models;
-using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
+        private ApplicationDbContext _context;
         public ViewResult Index()
         {
-            var movies = GetMovies();
+            var movies = GetMovies().ToList();
 
             return View(movies);    
         }
 
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        public ActionResult Details(int id)
+        {
+            var movie = GetMovies().SingleOrDefault(c => c.Id == id);
+
+            if (movie == null)
+                return HttpNotFound();
+
+            return View(movie);
+        }
+
         private IEnumerable<Movie> GetMovies()
         {
-            return new List<Movie>
-            {
-                new Movie { Id = 1, Name = "Shrek" },
-                new Movie { Id = 2, Name = "Wall-e" }
-            };
+            return _context.Movies.Include(g => g.Genre);
         }
 
         // GET: Movies/Random
         public ActionResult Random()
         {
-            var movie = new Movie() { Name = "Shrek!" };
-            var customers = new List<Customer>
-            {
-                new Customer { Name = "Customer 1" },
-                new Customer { Name = "Customer 2" }
-            };
+           
+            return View();
+        }
 
-            var viewModel = new RandomMovieViewModel
-            {
-                Movie = movie,
-                Customers = customers
-            };
-
-            return View(viewModel);
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
         }
     }
 }
--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -1,9 +1,15 @@
 ﻿
+using System;
+
 namespace Vidly.Models
 {
     public class Movie
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public Genre Genre { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public DateTime DateAdded { get; set; }
+        public int Stock { get; set; }
     }
 }
